170933a53431b344bec97c15f859cf16f3f79ab3
Marija Spasova <marija.spasova@aspekt.com.mk>
2024-04-04 09:46:37 +0200

    Added

---
+++ SQLProcedure/SQLProcedure.Models/Application.cs
@@ -0,0 +1,10 @@
+﻿namespace SQLProcedure.Models
+{
+    public class Application
+    {
+        public int ID { get; set; }
+        public Contact Contact { get; set; }
+        public int Status { get; set; }
+
+    }
+}
---
+++ SQLProcedure/SQLProcedure.Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLProcedure.Models
+{
+    public class Contact
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
